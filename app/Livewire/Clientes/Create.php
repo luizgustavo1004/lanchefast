7a55f6a1acebc159989e59fc53030348f6c4755f
luizgustavo1004 <senailuizgustavo@gmail.com>
2025-04-10 13:51:56 -0300

    feita o create dos clientes

--- app/Livewire/Clientes/Create.php
+++ app/Livewire/Clientes/Create.php
@@ -2,10 +2,38 @@
 
 namespace App\Livewire\Clientes;
 
+use App\Models\Cliente;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Create extends Component
 {
+
+    public $nome;
+    public $endereco;
+    public $telefone;
+    public $cpf;
+    public $email;
+    public $senha;
+
+    public function create()
+    {
+       
+
+        Cliente::create([
+            'nome' => $this->nome,
+            'endereco' => $this->endereco,
+            'telefone' => $this->telefone,
+            'cpf' => $this->cpf,
+            'email' => $this->email,
+            'senha' => Hash::make($this->senha),
+        ]);
+
+        
+        session()->flash('success', 'Cliente criado com sucesso!');
+    }
+
+
     public function render()
     {
         return view('livewire.clientes.create');
